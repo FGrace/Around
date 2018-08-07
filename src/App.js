a82fdb9f9e656f909299261e3f6d745220d853bd
FGrace <jiefang2012@u.northwestern.edu>
2018-08-07 00:18:05 -0400

    login-2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {Header} from './Header'
+import {Main} from './Main'
 
 class App extends Component {
   render() {
     return (
       <div className= "App" >
           <Header/>
+          <Main/>
       </div>
     );
   }
---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Register} from './Register'
+
+
+export class Main extends React.Component{
+
+    render(){
+        return (
+            <div>
+               <Register/>
+            </div>
+
+        );
+    }
+}
